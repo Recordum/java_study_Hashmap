9dbbe4d42f9c6246e673b90d34cf4f94870d299a
mingy <mingy@DevMin>
2022-11-20 23:56:54 +0900

    hashmap

--- src/HashMapEx.java
+++ src/HashMapEx.java
@@ -1,27 +1,59 @@
+
 import java.util.*;
 
+
 class HashMapEx {
+	
+	
 	public static void main(String[] args) {
+
 	
 	Scanner scanner = new Scanner(System.in);
-	
 	HashMap<String, Student> login = new HashMap<String, Student>();
-	
+	String name;
+	String test;
+
 	while (true)
 	{
 		
-		System.out.println("이름, ID(학번), PW(문자) 를 빈칸으로 나누어 입력하시오");
+		System.out.println("이름, ID(학번), PW(문자) 를 입력하세요");
 		
-		String name = scanner.next();
-		if(name == "종료")
-			{break;}
+		name = scanner.next();
+		if(name.equals("종료")) {
+			break;}
+		else {
 		int id = scanner.nextInt();
 		String pw = scanner.next();
 		login.put(name, new Student(id, pw));
-		
+		System.out.println("다음과 같이 학생정보가 등록되었습니다");
+		System.out.println("이름 : " +name);
+		System.out.println("학번 : " +login.get(name).getId());
+		System.out.println("비밀번호 : " +login.get(name).getPassword());
+		}
 		
 	}
 	
-	
+	while(true) {
+		System.out.println("이름을 입력하세요");
+		name = scanner.next();
+		String nameTest = "오민규";
+		
+		if(name.equals("종료")) 
+		{
+			break;
+		}
+		
+		if(login.get(name)== null) // login.get(name) name(key)에 value를 stack메모리에 리턴하기 때문에 null과 레퍼
+		{
+			System.out.println("등록되지 않은 학생입니다 이름을 다시 입력해주세요");
+			
+			
+		}
+		else {
+					
+			System.out.println("학번 : " +login.get(name).getId());
+			System.out.println("비밀번호 : " +login.get(name).getPassword());
+			}
+		}	
 }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,17 +1,21 @@
 import java.util.*;
+
 public class Student {
-	private int id;
-	private String pw;
 	
+	public int id;
+	private String pw;
+
 	public Student(int id , String pw)
 	{
 		this.id = id;
 		this.pw = pw;
 	}
 	public int getId()
-	{return id;}
+	{	return id;}
 	public String getPassword()
 	{return pw;}
 
-	}
 	
+
+	
+}
